dca33a24f68aebf429ac8df6475be057da6b3c3e
soulsam480 <soulsam480@hotmail.com>
2021-11-12 09:52:13 +0530

    chore: analyze app bundle

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Windi from 'vite-plugin-windicss';
 // import VueTypeImports from 'vite-plugin-vue-type-imports';
+import analyze from 'rollup-plugin-analyzer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 4000,
   },
-  plugins: [vue(), Windi()],
+  plugins: [vue(), Windi(), analyze()],
   build: {
-    minify: 'terser',
+    minify: false,
   },
   resolve: {
     alias: {
