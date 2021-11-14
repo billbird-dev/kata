4236981943a0f94de078161e52f342093411771e
soulsam480 <soulsam480@hotmail.com>
2021-11-14 14:36:13 +0530

    feat: setup better component management system, component props, add component

--- app/src/types/index.ts
+++ app/src/types/index.ts
@@ -2,14 +2,14 @@ import { HeaderInfoStyles, StyleTools } from 'kata';
 import { CombinedComponentConfig } from 'kata/src';
 
 export type componentNames = 'kata-header' | 'header-info' | 'header-logo';
-
+export type componentProps = Record<string, string | number | { [x: string]: any }>;
 export type CombinedConfig = { [x: string]: string | number | undefined } & CombinedComponentConfig;
 
 export interface Block {
   component: componentNames;
   config?: CombinedConfig;
   children: Block[];
-  props?: Record<string, { [x: string]: any }>;
+  props?: componentProps;
   id: string;
   parent: boolean;
 }
--- app/src/views/Index.vue
+++ app/src/views/Index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { Block, Classes, CombinedConfig, componentNames } from 'src/types';
+import { Block, Classes, componentNames } from 'src/types';
 import TreeNode from 'src/components/TreeNode.vue';
 import { randomId } from 'src/utils';
 import { CombinedComponentConfig } from '../../../lib/src';
 import { FMenu } from 'furikaeru';
+import { ref } from 'vue';
 
 type SectionName = 'header' | 'intro' | 'info' | 'items' | 'total-footer' | 'summary' | 'misc';
 
@@ -18,57 +19,17 @@ interface Section {
 
 type InvoiceSchema = { [x in SectionName]?: Section };
 
-let schema = $ref<{ [x in SectionName]?: Block }>({
+let schema = ref<{ [x in SectionName]?: Block }>({
   header: {
     component: 'kata-header',
-    children: [
-      // {
-      //   component: 'header-info',
-      //   props: {
-      //     data: {
-      //       name: 'Bangali Traders',
-      //       address: 'Home',
-      //       branch: 'BBSR',
-      //       gstin: 'ssss',
-      //       phone: '988989989',
-      //     },
-      //   },
-      //   id: randomId(),
-      // },
-    ],
+    children: [],
     parent: true,
     id: randomId(),
+    config: {
+      justify: 'between',
+    },
   },
 });
-
-// function addMore() {
-//   schema.push({
-//     component: 'kata-header',
-//     children: [
-//       {
-//         component: 'header-info',
-//         props: {
-//           data: {
-//             name: 'Bangali Traders',
-//             address: 'Home',
-//             branch: 'BBSR',
-//             gstin: 'ssss',
-//             phone: '988989989',
-//           },
-//         },
-//         id: randomId(),
-//       },
-//     ],
-//     id: randomId(),
-//   });
-// }
-
-// - component
-// - id
-// - children
-//   - kata block @type {Block}
-// - config @type {CombinedConfig}
-// - props -> any {}
 
 /**
 Kata schema
@@ -94,34 +55,64 @@ const INVOICE_SCHEMA: InvoiceSchema = {
   },
 };
 
+const COMPONENT_PROPS: { [C in componentNames]: Record<string, string | number | { [x: string]: any }> } = {
+  'header-info': {
+    data: {
+      name: 'Bangali Traders',
+      address: 'Home',
+      branch: 'BBSR',
+      gstin: 'ssss',
+      phone: '988989989',
+    },
+  },
+  'header-logo': {
+    imgSrc: 'https://sambitsahoo.com/vue-opinions.png',
+    alt: 'Logo',
+  },
+  'kata-header': {},
+};
+
 let loopableSchema = $computed(() => Object.entries(INVOICE_SCHEMA) as any as [SectionName, Section][]);
 
-function trasformComponentsToOptions(components: AcceptedComponents) {
-  return Object.keys(components).map((key) => ({ label: key, value: key }));
+function trasformComponentsToOptions(components: AcceptedComponents, sectionEls: Block[]) {
+  return Object.keys(components)
+    .map((key) => ({ label: key, value: key }))
+    .filter((e) => !sectionEls.some((el) => el.component === e.label));
 }
 
 let model = $ref('');
 
 function handleAddElement(element: string | number, sectionId: SectionName) {
-  element = element as any as componentNames;
+  element = element as componentNames;
 
-  if(!schema[sectionId]) return;
-  const slots = schema[sectionId]?.children.length;
+  const section = schema.value[sectionId];
 
-  // const rest = slots && slots > 0 ? INVOICE_SCHEMA[sectionId]?.max -   
+  console.log(section);
 
-  // track how much filled
-  // check max - filled
-  // construct block
-  // append to parent
+  if (!section) return;
 
-  console.log(element, sectionId);
+  const slots = section.children.length;
+  const max = INVOICE_SCHEMA[sectionId]?.max || 0;
+
+  if (slots > 0 && max - slots === 0) return;
+
+  const newEl: Block = {
+    component: element as componentNames,
+    props: {
+      ...COMPONENT_PROPS[element as componentNames],
+    },
+    id: randomId(),
+    children: [],
+    parent: false,
+  };
+
+  schema.value[sectionId]?.children.push(newEl);
 }
 </script>
 <template>
   <div class="p-3 flex space-x-3 h-full max-h-full">
     <div class="w-1/3 flex-none">
-      <pre class="text-xs text-gray-800 p-2 rounded-md bg-gray-200">
+      <pre class="text-xs text-gray-800 p-2 rounded-md bg-gray-200 !break-all !break-words">
   {{ schema }}
     </pre
       >
@@ -142,9 +133,6 @@ function handleAddElement(element: string | number, sectionId: SectionName) {
           class="
             absolute
             inset-0
-            flex
-            items-center
-            justify-center
             bg-gray-200
             rounded-md
             duration-300
@@ -152,18 +140,45 @@ function handleAddElement(element: string | number, sectionId: SectionName) {
             ease-in-out
             hover:opacity-100
             group
+            flex-col
+            space-y-1
             opacity-0
+            py-1
           "
         >
-          <div class="p-2 transition-opacity duration-300 group-hover:opacity-100 opacity-0">
-            <f-menu
-              :options="trasformComponentsToOptions(section[1].components)"
-              option-key="value"
-              label="Add element"
-              v-model="model"
-              :sm="true"
-              @input="handleAddElement($event, section[0])"
-            />
+          <div class="text-center text-gray-500 font-semibold uppercase">{{ section[0] }}</div>
+          <div class="grid grid-cols-2 space-x-3 p-2">
+            <!-- //? manage components container -->
+            <div class="flex-grow flex space-x-1 justify-between text-xs">
+              <div class="flex-col space-y-1">
+                <div>Add component</div>
+                <div>current components count: {{ schema[section[0]]?.children.length }}</div>
+              </div>
+              <f-menu
+                :options="trasformComponentsToOptions(section[1].components, schema[section[0]]?.children || [])"
+                option-key="value"
+                label="Add element"
+                v-model="model"
+                :sm="true"
+                @input="handleAddElement($event, section[0])"
+                v-if="schema[section[0]]?.children.length !== INVOICE_SCHEMA[section[0]]?.max"
+              />
+            </div>
+
+            <!-- //? child config container -->
+            <div class="flex-grow flex space-x-1 justify-between text-xs">
+              <div class="flex-col space-y-1">
+                <div>Config</div>
+              </div>
+              <!-- <f-menu
+                :options="trasformComponentsToOptions(section[1].components, schema[section[0]]?.children || [])"
+                option-key="value"
+                label="Add element"
+                v-model="model"
+                :sm="true"
+                @input="handleAddElement($event, section[0])"
+              /> -->
+            </div>
           </div>
         </div>
 
@@ -172,4 +187,3 @@ function handleAddElement(element: string | number, sectionId: SectionName) {
     </div>
   </div>
 </template>
-<style lang="scss" scoped></style>
